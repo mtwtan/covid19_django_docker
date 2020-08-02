fda676165635e16eef14e810da94ee76e10d34ac
mtwtan <mtwtan@gmail.com>
2020-08-02 16:41:49 -0400

    Update settings.py

--- covid19/settings.py
+++ covid19/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hevd3y!i43=h3qj(j@#58@lh&lw#80okfirb#$3h2tvs7rr7io'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
