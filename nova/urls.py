ac7b0b52b9abbfb9b9ca25c1f0e984433fa646d3
Matthew Tan <mtwtan@gmail.com>
2020-08-02 15:12:30 -0400

    views modified

--- nova/urls.py
+++ nova/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('plt_nova_total_cases', views.plt_nova_view, name='plt_nova_total_cases_view'),
+    path('plt_nova_total_cases', views.plt_nova_total_cases_view, name='plt_nova_total_cases_view'),
 ]
