0518ad2d1c0ffb3b2a1c33e1da4a681b9b493eea
Matthew Tan <mtwtan@gmail.com>
2020-08-04 17:24:52 -0400

    nova charts

--- nova/urls.py
+++ nova/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('plt_nova_total_cases', views.plt_nova_total_cases_view, name='plt_nova_total_cases_view'),
+    path('plt_nova_casechange', views.plt_nova_casechange_view, name='plt_nova_casechange_view'),
+    path('plt_nova_mavg', views.plt_nova_movingavg_view, name='plt_nova_movingavg'),
+    path('plt_nova_changedeath_view', views.plt_nova_changedeath_view, name='plt_nova_changedeath_view'),
+    path('plt_nova_movingavg_deaths_view', views.plt_nova_movingavg_deaths_view, name='plt_nova_movingavg_deaths_view'),
 ]
--- nova/views.py
+++ nova/views.py
@@ -74,3 +74,153 @@ def plt_nova_total_cases_view(request):
   fig_buffer.close()
 
   return response
+
+def plt_nova_movingavg_view(request):
+#  df_nova = pd.DataFrame(list(DmvMovingAverage.objects.using('data').all().values()))
+#  df_nova['date'] = pd.to_datetime(df_nova['date'])
+#  df_index = df_nova.index
+#  num_rows = len(df_index)
+#  max_index = num_rows - 1
+
+  fig, ax = plt.subplots()
+  ax.set_title('7 days Moving Average - Confirmed Cases')
+  ax.plot('date', 'movingaverage', data=df_nova.loc[df_nova['county'] == 'Fairfax'], label="Fairfax")
+  ax.plot('date', 'movingaverage', data=df_nova.loc[df_nova['county'] == 'Arlington'], label="Arlington")
+  ax.plot('date', 'movingaverage', data=df_nova.loc[df_nova['county'] == 'District of Columbia'], label="District of Columbia")
+  ax.plot('date', 'movingaverage', data=df_nova.loc[df_nova['county'] == 'Montgomery'], label="Montgomery")
+  ax.plot('date', 'movingaverage', data=df_nova.loc[df_nova['county'] == 'Loudoun'], label="Loudoun")
+  ax.plot('date', 'movingaverage', data=df_nova.loc[df_nova['county'] == 'Alexandria'], label="Alexandria")
+
+  ax.xaxis.set_major_locator(months)
+  ax.xaxis.set_major_formatter(month_fmt)
+  ax.xaxis.set_minor_locator(days)
+
+#  # round to nearest months
+  datemin = np.datetime64(df_nova['date'][0], 'D')
+#  datemax = np.datetime64(df_fairfax['date'][-1], 'D') + np.timedelta64(15, 'D')
+  datemax = np.datetime64(df_nova['date'][max_index], 'D')
+  ax.set_xlim(datemin, datemax)
+
+  plt.tight_layout()
+  plt.text
+  plt.legend()
+  fig_buffer = BytesIO()
+  plt.savefig(fig_buffer, format='png', dpi=150)
+
+  # Save the figure as a HttpPesponse
+  response = HttpResponse(content_type='image/png')
+  response.write(fig_buffer.getvalue())
+
+  fig_buffer.close()
+
+  return response
+
+def plt_nova_casechange_view(request):
+#  df_nova = pd.DataFrame(list(DmvMovingAverage.objects.using('data').all().values()))
+#  df_nova['date'] = pd.to_datetime(df_nova['date'])
+#  df_index = df_nova.index
+#  num_rows = len(df_index)
+#  max_index = num_rows - 1
+
+  fig, ax = plt.subplots()
+  ax.set_title('Confirmed Cases - Daily')
+  ax.plot('date', 'change', data=df_nova.loc[df_nova['county'] == 'Fairfax'], label="Fairfax")
+  ax.plot('date', 'change', data=df_nova.loc[df_nova['county'] == 'Arlington'], label="Arlington")
+  ax.plot('date', 'change', data=df_nova.loc[df_nova['county'] == 'District of Columbia'], label="District of Columbia")
+  ax.plot('date', 'change', data=df_nova.loc[df_nova['county'] == 'Montgomery'], label="Montgomery")
+  ax.plot('date', 'change', data=df_nova.loc[df_nova['county'] == 'Loudoun'], label="Loudoun")
+  ax.plot('date', 'change', data=df_nova.loc[df_nova['county'] == 'Alexandria'], label="Alexandria")
+
+  ax.xaxis.set_major_locator(months)
+  ax.xaxis.set_major_formatter(month_fmt)
+  ax.xaxis.set_minor_locator(days)
+
+#  # round to nearest months
+  datemin = np.datetime64(df_nova['date'][0], 'D')
+#  datemax = np.datetime64(df_fairfax['date'][-1], 'D') + np.timedelta64(15, 'D')
+  datemax = np.datetime64(df_nova['date'][max_index], 'D')
+  ax.set_xlim(datemin, datemax)
+
+  plt.tight_layout()
+  plt.text
+  plt.legend()
+  fig_buffer = BytesIO()
+  plt.savefig(fig_buffer, format='png', dpi=150)
+
+  # Save the figure as a HttpPesponse
+  response = HttpResponse(content_type='image/png')
+  response.write(fig_buffer.getvalue())
+
+  fig_buffer.close()
+
+  return response
+
+def plt_nova_changedeath_view(request):
+
+  fig, ax = plt.subplots()
+  ax.set_title('Deaths - Daily')
+  ax.plot('date', 'changedeaths', data=df_nova.loc[df_nova['county'] == 'Fairfax'], label="Fairfax")
+  ax.plot('date', 'changedeaths', data=df_nova.loc[df_nova['county'] == 'Arlington'], label="Arlington")
+  ax.plot('date', 'changedeaths', data=df_nova.loc[df_nova['county'] == 'District of Columbia'], label="District of Columbia")
+  ax.plot('date', 'changedeaths', data=df_nova.loc[df_nova['county'] == 'Montgomery'], label="Montgomery")
+  ax.plot('date', 'changedeaths', data=df_nova.loc[df_nova['county'] == 'Loudoun'], label="Loudoun")
+  ax.plot('date', 'changedeaths', data=df_nova.loc[df_nova['county'] == 'Alexandria'], label="Alexandria")
+
+  ax.xaxis.set_major_locator(months)
+  ax.xaxis.set_major_formatter(month_fmt)
+  ax.xaxis.set_minor_locator(days)
+
+#  # round to nearest months
+  datemin = np.datetime64(df_nova['date'][0], 'D')
+#  datemax = np.datetime64(df_fairfax['date'][-1], 'D') + np.timedelta64(15, 'D')
+  datemax = np.datetime64(df_nova['date'][max_index], 'D')
+  ax.set_xlim(datemin, datemax)
+
+  plt.tight_layout()
+  plt.text
+  plt.legend()
+  fig_buffer = BytesIO()
+  plt.savefig(fig_buffer, format='png', dpi=150)
+
+  # Save the figure as a HttpPesponse
+  response = HttpResponse(content_type='image/png')
+  response.write(fig_buffer.getvalue())
+
+  fig_buffer.close()
+
+  return response
+
+def plt_nova_movingavg_deaths_view(request):
+
+  fig, ax = plt.subplots()
+  ax.set_title('7 days Moving Average - Deaths')
+  ax.plot('date', 'movingaveragedeaths', data=df_nova.loc[df_nova['county'] == 'Fairfax'], label="Fairfax")
+  ax.plot('date', 'movingaveragedeaths', data=df_nova.loc[df_nova['county'] == 'Arlington'], label="Arlington")
+  ax.plot('date', 'movingaveragedeaths', data=df_nova.loc[df_nova['county'] == 'District of Columbia'], label="District of Columbia")
+  ax.plot('date', 'movingaveragedeaths', data=df_nova.loc[df_nova['county'] == 'Montgomery'], label="Montgomery")
+  ax.plot('date', 'movingaveragedeaths', data=df_nova.loc[df_nova['county'] == 'Loudoun'], label="Loudoun")
+  ax.plot('date', 'movingaveragedeaths', data=df_nova.loc[df_nova['county'] == 'Alexandria'], label="Alexandria")
+
+  ax.xaxis.set_major_locator(months)
+  ax.xaxis.set_major_formatter(month_fmt)
+  ax.xaxis.set_minor_locator(days)
+
+#  # round to nearest months
+  datemin = np.datetime64(df_nova['date'][0], 'D')
+#  datemax = np.datetime64(df_fairfax['date'][-1], 'D') + np.timedelta64(15, 'D')
+  datemax = np.datetime64(df_nova['date'][max_index], 'D')
+  ax.set_xlim(datemin, datemax)
+
+  plt.tight_layout()
+  plt.text
+  plt.legend()
+  fig_buffer = BytesIO()
+  plt.savefig(fig_buffer, format='png', dpi=150)
+
+  # Save the figure as a HttpPesponse
+  response = HttpResponse(content_type='image/png')
+  response.write(fig_buffer.getvalue())
+
+  fig_buffer.close()
+
+  return response
