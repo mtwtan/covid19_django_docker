7037085e521031f37545a55fd2970d069b638007
Matthew Tan <mtwtan@gmail.com>
2020-08-06 16:38:07 -0400

    table count

--- nova/urls.py
+++ nova/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('plt_nova_changedeath_view', views.plt_nova_changedeath_view, name='plt_nova_changedeath_view'),
     path('plt_nova_movingavg_deaths_view', views.plt_nova_movingavg_deaths_view, name='plt_nova_movingavg_deaths_view'),
     path('tbl_nova', views.datatable, name='tbl_nova'),
+    path('tbl_nova_count', views.datatable_count, name='tbl_nova_count'),
     path('list_counties', views.counties_json, name='list_counties'),
 ]
